517eaa8c60d13f278c407ab490bf2ddc39909715
Alexandre Chambet <alex.chambet@gmail.com>
2019-10-28 20:14:36 +0100

    Users can update their information

--- webserver/src/Controller/UserController.php
+++ webserver/src/Controller/UserController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\Type\UserType;
+use App\Security\Encoder\ShaPasswordEncoder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/user/", name="user_home")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('user/user_home.html.twig');
-    }
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+
+            // Encode the password
+            $encoder = new ShaPasswordEncoder();
+            $encoded = $encoder->encodePassword($user->getPassword(), null);
+            $user->setPwd($encoded);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Your personal data have been updated.');
+        }
+
+        return $this->render('user/user_home.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 }
